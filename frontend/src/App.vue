26f4c395f964282b001af56b4fc8cb716976a1cd
Javad Hamidi <19621911+javadhamidi@users.noreply.github.com>
2022-05-21 21:34:06 +0930

    👨‍💻📦

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,76 @@
 <script setup>
 import Debug from "./components/Debug.vue";
+import ClassCard from "./components/ClassCard.vue";
 </script>
 
 <template>
-    <Debug />
+    <header>
+        <button><img src="/assets/arrow-left.svg" alt="Back arrow"></button>
+        <h1 class="title">Practical 15069</h1>
+        <button><img src="/assets/cog.svg" alt="Settings icon"></button>
+    </header>
+
+    <main>
+        <ClassCard title="Workshop WR02" details="4210_COMP_SCI_X_0019" />
+
+        <Debug />
+    </main>
+
 </template>
 
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
+    @import url("https://fonts.googleapis.com/css?family=Space+Grotesk:400,500,700&display=swap");
+
+    #app {
+        /* -webkit-font-smoothing: antialiased; */
+        /* -moz-osx-font-smoothing: grayscale; */
+    }
+
+    * {
+        box-sizing: border-box;
+
+        font-family: "Space Grotesk", sans-serif;
+    }
+
+    body, h1, h2, p {
+        margin: 0;
+    }
+
+    h2 {
+        font-size: 16px;
+    }
+
+    p {
+        font-size: 14px;
+    }
+
+    header {
+        display: flex;
+    }
+
+    header button {
+        margin-bottom: 16px;
+        
+        display: inline-block;
+        border: none;
+        background-color: transparent;
+
+        height: 64px;
+        width: 64px;
+        padding: 20px;
+        border-radius: 20px;
+    }
+
+    .title {
+        font-size: 24px;
+        display: inline-block;
+        height: 32px;
+        margin-top: 16px;
+        margin-bottom: 32px;
+        flex: 1;
+    }
+
+    main {
+        margin: 0 28px 32px 28px;
+    }
 </style>
---
+++ frontend/src/components/ClassCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+    defineProps({
+        title: {
+            type: String,
+            default: "",
+        },
+        details: {
+            type: String,
+            default: "",
+        },
+    });
+</script>
+
+<template>
+    <div class="card">
+        <div class="card-heading">
+            <div class="card-text">
+                <h2>{{ title }}</h2>
+            </div>
+        </div>
+        <div>
+            <div class="card-text">
+                <p>{{ details }}</p>
+            </div>
+        </div>
+        <div class="navigation">
+            <button style="flex-grow: 1;">Hello</button>
+            <button style="flex-grow: 2;">Hello</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .card {
+        display: flex;
+        flex-direction: column;
+
+        border: 2px solid black;
+        border-radius: 5px;
+        height: 176px;
+        padding: 16px;
+        margin-bottom: 16px;
+    }
+
+    .card button {
+        border: 2px solid black;
+        border-radius: 20px;
+    }
+
+    .card-text {
+        display: table-cell;
+        vertical-align: middle;
+        font-size: 16px;
+        height: 28px;
+    }
+
+    .card-heading {
+        margin-bottom: 8px;
+    }
+
+    .navigation {
+        display: flex;
+        gap: 16px;
+        margin-top: auto;
+    }
+
+    .navigation button {
+        height: 28px;
+    }
+</style>
