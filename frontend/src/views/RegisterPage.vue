3b4715dff2e83d32a7ab9cb21c427ceafb8f5c2b
FlakeVer <70008267+FlakeVer@users.noreply.github.com>
2022-06-07 06:56:27 +0000

    Tue, Jun 7, 2022, 6:56 AM +00:00

--- frontend/src/views/RegisterPage.vue
+++ frontend/src/views/RegisterPage.vue
@@ -85,6 +85,7 @@ const isFormValid = computed(
         </div>
         <div class="spacer" />
         <div class="confirmation-buttons">
+        <router-link to='/LoginPage.vue'>
             <button
                 class="confirmation-confirm"
                 :disabled="submitting || !isFormValid"
@@ -93,6 +94,7 @@ const isFormValid = computed(
                 <img src="/assets/check-circle.svg" alt="Back arrow" />
                 <p>Register</p>
             </button>
+            </router-link>
         </div>
     </div>
 </template>
